90d15e96c78a2d127677f8f5f0e9a1463d9a381a
Polina Zakaryan <zakaryan.polina@yandex.ru>
2019-03-26 23:20:07 +0300

    correct task2_5

--- task2/task2_5.js
+++ task2/task2_5.js
@@ -1,4 +1,14 @@
-function getMaxOfArray(arr) {
-    return Math.max(...arr);
+// function getMaxOfArray(arr) {
+//     return Math.max(...arr);
+//     };
+//     console.log(getMaxOfArray([1, 4, 5, -5, 10, -44, 0]));
+
+    var getMax = function (array) {
+        var max = array[0];
+        for (var i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
     };
-    console.log(getMaxOfArray([1, 4, 5, -5, 10, -44, 0]));
