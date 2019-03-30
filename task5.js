f134004f11f58036086c2e9e4c643336040c91c8
Polina Zakaryan <zakaryan.polina@yandex.ru>
2019-03-30 23:32:09 +0300

    add task5(calculator)

---
+++ task5.js
@@ -0,0 +1,37 @@
+var currentValue = 0;
+var Calculator = function () {
+	this.add = function (a) {
+		currentValue += a;
+		return currentValue;
+				
+    };
+	this.subtract = function (a) {
+		currentValue -= a;
+		return currentValue;
+		
+    };
+	this.divide = function (a) {
+		currentValue /= a;
+		return currentValue;
+    };
+	this.multiply = function (a) {
+		currentValue *= a;
+		return currentValue;
+    };
+	this.getResult = function() {
+		return currentValue;
+    };
+	this.reset = function () {
+		currentValue = 0;
+		return currentValue;
+    }
+	
+}
+var example = new Calculator();
+example.add(3);
+example.subtract(2);
+example.multiply(20);
+example.divide(4);
+example.getResult();
+example.reset();
+example.add(100);
