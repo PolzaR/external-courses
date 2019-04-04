0ec05acb972a8110bd8b4329289278196bb63fcf
Polina Zakaryan <zakaryan.polina@yandex.ru>
2019-04-04 18:56:09 +0300

    correct task5

--- task5/task5.js
+++ task5/task5.js
@@ -1,138 +1,49 @@
-//Я пока не нашла правильного решения задачи, но это тоже работает.
-var currentValue = 0;
-var Calculator = function () {
-	this.add = function (a) {
-		currentValue += a;
-		return currentValue;
-				
-    };
-	this.subtract = function (a) {
-		currentValue -= a;
-		return currentValue;
-		
-    };
-	this.divide = function (a) {
-		currentValue /= a;
-		return currentValue;
-    };
-	this.multiply = function (a) {
-		currentValue *= a;
-		return currentValue;
-    };
-	this.getResult = function() {
-		return currentValue;
-    };
-	this.reset = function () {
-		currentValue = 0;
-		return currentValue;
-    }
-	
-}
-var example = new Calculator();
-example.add(3);
-example.subtract(2);
-example.multiply(20);
-example.divide(4);
-example.getResult();
-example.reset();
-example.add(100);
-
-
-
-var Calculator = (function(x) {
+var Calculator = (function() {
 	var currentValue = 0;
 		
 	return {
-		add: function(x) {
-			return currentValue += x;
+		add: function add(x) {
+			if ( typeof (x) == "undefined" ) {
+				x = 0;
+			};
+			currentValue = currentValue + x;
+			return add;
+		},
+		subtract: function subtract(x) {
+			if ( typeof (x) == "undefined" ) {
+				x = 0;
+			};
+			currentValue = currentValue - x;
+			return subtract;
         },
-		subtract: function(x) {
-			return currentValue -= x;
+		multiply: function multiply(x) {
+			if ( typeof (x) == "undefined" ) {
+				x = 0;
+			};
+			currentValue = currentValue * x;
+			return multiply;
         },
-		multiply: function(x) {
-			return currentValue *= x;
+		divide: function divide(x) {
+			if ( typeof (x) == "undefined" ) {
+				x = 0;
+			};
+			currentValue = currentValue / x;
+			return divide;
         },
-		divide: function(x) {
-			return currentValue /= x;
-        },
-		getResult: function() {
+		getResult: function getResult() {
 			return currentValue;
         },
-		reset: function() {
-			currentValue = 0;
-			return currentValue;
-        }
-    }
-})();
-Calculator.add(90);
-Calculator.subtract(20);
-Calculator.multiply(2);
-Calculator.divide(7);
-Calculator.getResult();
-Calculator.reset();
-Calculator.add(21);
-Calculator.getResult();
-
-var Calculator = (function(x) {
-	var currentValue = 0;
-		
-	return {
-		add: function(x) {
-			return function(y) {
-				return currentValue = currentValue + y + x;
-            }
-        },
-		subtract: function(x) {
-			return currentValue -= x;
-        },
-		multiply: function(x) {
-			return currentValue *= x;
-        },
-		divide: function(x) {
-			return currentValue /= x;
-        },
-		getResult: function() {
-			return currentValue;
-        },
-		reset: function() {
+		reset: function reset() {
 			currentValue = 0;
 			return currentValue;
         }
     }
 })();
 
-
-
-var Calculator = (function(x) {
-	var currentValue = 0;
-		
-	return {
-		add: function(x) {
-			currentValue = x;
-			function f(y) {
-				if (y === undefined) {
-					return currentValue;
-                } else {
-					currentValue += y;
-					return second;
-				}
-            }
-        },
-		subtract: function(x) {
-			return currentValue -= x;
-        },
-		multiply: function(x) {
-			return currentValue *= x;
-        },
-		divide: function(x) {
-			return currentValue /= x;
-        },
-		getResult: function() {
-			return currentValue;
-        },
-		reset: function() {
-			currentValue = 0;
-			return currentValue;
-        }
-    }
-})();
+Calculator.add(4);
+Calculator.subtract(1);
+Calculator.add(4)(1);
+console.log(Calculator.getResult())
+Calculator.add()();
+console.log(Calculator.reset());
+Calculator.subtract(1)(1)(1)(2);
