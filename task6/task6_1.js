12d7d353ded3e54811b5dff661a6829a9dbc2200
Polina Zakaryan <zakaryan.polina@yandex.ru>
2019-04-11 23:43:28 +0300

    correct task6_1

--- task6/task6_1.js
+++ task6/task6_1.js
@@ -1,29 +1,24 @@
 function sliceAnalogy (array, begin, end) {
-	result = [];
+	var result = [];
+
 	if ((typeof begin === "undefined") && (typeof end === "undefined")) {
 		result = array;
-    }
-	else if (typeof end !== "undefined") {
-		if ((begin < 0) && (end < 0)) {
-			for (var i = array.length + begin; i < array.length + end; i++) {
-				result[i] = array[i]
-            }
-        } else if (end < 0) {
-			for (var i = begin; i < array.length + end; i++) {
-				result[i] = array[i]
-            }
-        } else if (begin < 0) {
-			for (var i = array.length + begin; i < end; i++) {
-				result[i] = array[i]
-            } 
-        } else {
-			for (var i = begin; i < end; i++) {
-				result[i] = array[i];
-            }
-  	    }
-    } else if (typeof end === "undefined") {		
-		for (var i = begin; i < array.length; i++) {
+    };
+	if (typeof begin === "undefined") {
+		begin = 0;
+    };
+	if (typeof end === "undefined") {
+		end = array.length;
+	};
+	if (begin < 0) {
+		begin = array.length + begin;
+	}; 
+	if (end < 0) {
+		end = array.length + end;
+	};
+    for (var i = begin; i < end; i++) {
 			result[i] = array[i]
-        }
-    } return result;
+		}
+		
+    return result;
 }
